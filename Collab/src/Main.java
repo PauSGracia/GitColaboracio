c6ea9f3ecfa82218cd39028e1361db25cd3aadd2
HectorGarciaCarlota <soyunpenejaja@gmail.com>
2024-10-11 10:02:39 +0200

    he fet cosillas

--- Collab/src/Main.java
+++ Collab/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean yeye= true;
+        if (yeye == true) {
+            System.out.println("yeyeyeyeyeyeyeyeyeeyeyeyeyeyeyy");
+        }
     }
+
+
 }
