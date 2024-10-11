1d7882d40076fc0c0a07ab4ada7d62420a48ccb0
Pau <pau.sole@gracia.lasalle.cat>
2024-10-11 10:14:09 +0200

    clena code

--- Collab/src/Main.java
+++ Collab/src/Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
         boolean yeye= true;
-        if (yeye == true) {
-            System.out.println("yeyeyeyeyeyeyeyeyeeyeyeyeyeyeyy");
-        }
-        int name=0;
+        String name;
         System.out.println("Type your name");
+        name = input.nextLine();
+
+        if (yeye) {
+            System.out.println("Your name is: " + name + " yeyeyeyeyeyeyeyeyeeyeyeyeyeyeyy");
+        }
+
     }
 
 
